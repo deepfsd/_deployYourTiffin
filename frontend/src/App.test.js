00ff3fdad22fcd74a8d7796cb480ef5d2bb135cb
Deepanshu <ansudeep695@gmail.com>
2023-12-19 02:16:53 +0530

    more app.test.js changes (bro bhaut error aa rhe hai)

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,13 @@
+import React from 'react'; // Import React
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
